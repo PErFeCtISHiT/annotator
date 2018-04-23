3db5c90c16bb485c73de789e2cdffa98b3971a7f
Cao Jiawei <1093797485@qq.com>
2018-04-23 09:48:35 +0800

    navigation bar

---
+++ annotator/src/components/firstcomponent.vue
@@ -0,0 +1,21 @@
+<<template>
+    <div id="firstComponent">
+        <h1>I am a title</h1>
+        <a>written by {{ author }}</a>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+      return {
+          author: "Keith"
+      }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
--- annotator/src/router/index_router.js
+++ annotator/src/router/index_router.js
@@ -3,7 +3,18 @@ import VueRouter from 'vue-router'
 
 
 Vue.use(VueRouter);
+import first from '../components/firstcomponent'
+import second from '../components/secondcomponent'
 
 export default new VueRouter({
-
+  routes: [
+    {
+      path: "/1",
+      component: first
+    },
+    {
+      path: "/4",
+      component: second
+    }
+  ]
 });
