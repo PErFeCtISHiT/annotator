84c5369d533a5e0c5a6206cdedc695b7debd00d7
kiki <7988388122qq.com>
2018-04-24 11:09:56 +0800

    admin ui

--- annotator/src/router/index_router.js
+++ annotator/src/router/index_router.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 import first from '../components/firstcomponent'
 import distributeNewTask from '../components/distributeNewTask'
 import testDraw from '../components/testDraw'
+import admin from '../components/admin'
 
 export default new VueRouter({
   routes: [
@@ -16,6 +17,10 @@ export default new VueRouter({
     {
       path: "/1-2",
       component: distributeNewTask
+    },
+    {
+      path: "/3-1",
+      component: admin
     },
     {
       path: '/testDraw',
