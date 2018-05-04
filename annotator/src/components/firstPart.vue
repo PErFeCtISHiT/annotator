7084eb0ebbea83da3a103c73ce3107eafd60fa1f
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2018-05-04 11:28:41 +0800

    主动识别

--- annotator/src/components/firstPart.vue
+++ annotator/src/components/firstPart.vue
@@ -1,41 +1,43 @@
 <template>
   <el-col :span="24">
 
-    <img src="../images/main.png" width="100%">
+    <div id = "firstPageInput"></div>
 
-    <br>
-    <br>
+    <!--<img src="../images/main.png" width="100%">-->
 
-    <el-row style="background-color: #e3eef3; border-radius: 10px">
-      <el-col :span="1">
-        <img src="../images/advantage.png" width="60" style="margin-top: 2px">
-      </el-col>
-      <el-col :span="22" style="margin-top: 14px; margin-left: 20px;">
-        <div style="font-size: 25px; color: #343434; font-weight: bolder">我们的优势</div>
-      </el-col>
-    </el-row>
+    <!--<br>-->
+    <!--<br>-->
 
-    <br>
-    <img src="../images/team.png" width="33%">
-    <img src="../images/data.png" width="33%">
-    <img src="../images/sys.png" width="33%">
+    <!--<el-row style="background-color: #e3eef3; border-radius: 10px">-->
+      <!--<el-col :span="1">-->
+        <!--<img src="../images/advantage.png" width="60" style="margin-top: 2px">-->
+      <!--</el-col>-->
+      <!--<el-col :span="22" style="margin-top: 14px; margin-left: 20px;">-->
+        <!--<div style="font-size: 25px; color: #343434; font-weight: bolder">我们的优势</div>-->
+      <!--</el-col>-->
+    <!--</el-row>-->
 
-    <br>
-    <br>
+    <!--<br>-->
+    <!--<img src="../images/team.png" width="33%">-->
+    <!--<img src="../images/data.png" width="33%">-->
+    <!--<img src="../images/sys.png" width="33%">-->
 
-    <el-row style="background-color: #e3eef3; border-radius: 10px">
-      <el-col :span="1">
-        <img src="../images/pen.png" width="60" style="margin-top: 2px">
-      </el-col>
-      <el-col :span="22" style="margin-top: 14px; margin-left: 20px;">
-        <div style="font-size: 25px; color: #343434; font-weight: bolder">多样化标注方式</div>
-      </el-col>
-    </el-row>
-    <br>
+    <!--<br>-->
+    <!--<br>-->
 
-    <img src="../images/simple.png" width="33%">
-    <img src="../images/part.png" width="33%">
-    <img src="../images/total.png" width="33%">
+    <!--<el-row style="background-color: #e3eef3; border-radius: 10px">-->
+      <!--<el-col :span="1">-->
+        <!--<img src="../images/pen.png" width="60" style="margin-top: 2px">-->
+      <!--</el-col>-->
+      <!--<el-col :span="22" style="margin-top: 14px; margin-left: 20px;">-->
+        <!--<div style="font-size: 25px; color: #343434; font-weight: bolder">多样化标注方式</div>-->
+      <!--</el-col>-->
+    <!--</el-row>-->
+    <!--<br>-->
+
+    <!--<img src="../images/simple.png" width="33%">-->
+    <!--<img src="../images/part.png" width="33%">-->
+    <!--<img src="../images/total.png" width="33%">-->
   </el-col>
 
 
@@ -43,7 +45,20 @@
 
 <script>
   export default {
-    name: "first-part"
+    name: "first-part",
+
+    mounted(){
+      this.initFirstPage();
+    },
+
+    methods:{
+      initFirstPage(){
+        let area = $("#firstPageInput");
+        area.empty();
+        area.load('../../src/firstPage/index.html');
+
+      }
+    }
   }
 </script>
 
