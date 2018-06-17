3bb5910347fc95f3408255172042b3db9cefbeb5
Cao Jiawei <1093797485@qq.com>
2018-06-17 11:38:49 +0800

    发布任务这块

--- annotator/src/components/requester/requesterTaskItem.vue
+++ annotator/src/components/requester/requesterTaskItem.vue
@@ -1,9 +1,9 @@
 <template>
 
 
-    <el-col :span="8" class="slot-border">
-      <!-- 最外面一层是给引用它的都看的 -->
-      <div>
+  <el-col :span="8" class="slot-border">
+    <!-- 最外面一层是给引用它的都看的 -->
+    <div>
       <el-col :span="24">
 
         <!-- 主体 -->
@@ -16,13 +16,13 @@
                 <img src="../../images/task.png" style="width: 55px; height: 55px"/>
               </el-col>
               <el-col :span="17">
-                <el-button type="text" @click="jump">
-                  <div style="font-size: larger;">任务名称</div>
-                  <div
-                    style="text-align: center; margin-right: 20px; margin-top: 14px; font-size: larger; font-weight: bolder;">
-                    {{ taskMsg.taskName }}
-                  </div>
-                </el-button>
+                <!--<el-button type="text" @click="jump">-->
+                <div style="font-size: larger;">任务名称</div>
+                <div
+                  style="text-align: center; margin-right: 20px; margin-top: 14px; font-size: larger; font-weight: bolder;">
+                  {{ taskMsg.taskName }}
+                </div>
+                <!--</el-button>-->
               </el-col>
               <!--<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>-->
             </div>
@@ -32,29 +32,33 @@
                 <el-col :span="8" style="margin-top: 23px">
                   <span class="label-all">任务编号:</span>
                 </el-col>
-                <el-col :span="13" class="label-all" style="text-align: right; margin-right: 10px">
+                <el-col :span="13" class="label-all" style="text-align: right">
                   <span class="taskIDLabel">NO.</span>
                   <span class="taskIDLabel" id="taskID">{{ taskMsg.taskID }}</span>
                 </el-col>
               </div>
+
               <div>
                 <el-col :span="8" class="label-all" style="margin-top: 45px">
                   <span>任务描述:</span>
                 </el-col>
-                <el-col :span="14" class="label-all label_detail" style="text-align: left; height: 100px">
-                  <span>{{ taskMsg.description }}</span>
+                <el-col :span="15" class="label-all label_detail">
+                  <div style="text-align: center; margin: auto; height: 105px;
+                  box-shadow: 5px 5px 5px #9d9d9d; padding: 5px 5px 5px 5px; background-color: rgba(223,248,251,0.35)">
+                    <span style="padding-top: 10px">{{ taskMsg.description }}</span>
+                  </div>
                 </el-col>
               </div>
+
               <div>
-                <el-col :span="8" class="label-all" style="margin-top: 17px">
+                <el-col :span="8" class="label-all" style="margin-top: 30px">
                   <span>任务标签:</span>
                 </el-col>
-                <el-col :span="12" class="label-all label_detail">
-                  <span v-for="theTag in taskMsg.tag">
-                    <el-tag >{{ theTag }}</el-tag>&thinsp;
-                  </span>
+                <el-col :span="14" class="label-all label_detail">
+                  <el-tag v-for="theTag in taskMsg.tag" style="margin: 10px 10px 10px 10px">{{ theTag }}</el-tag>&thinsp;
                 </el-col>
               </div>
+
               <div>
                 <el-col :span="10" class="label-all">
                   <span>发布时间:</span>
@@ -74,24 +78,35 @@
               </div>
 
               <div>
-                <el-col :span="10" class="label-all" style="margin-bottom: 20px">
-                  <span>任务进度:</span>
+                <el-col :span="10" class="label-all">
+                  <span>图片数量:</span>
                 </el-col>
-                <el-col :span="12" class="label-all">
-                  <el-progress :text-inside="true" :stroke-width="18" :percentage="taskMsg.totalProgress.toFixed(2) * 100" :status="taskMsg.totalProgress===1?'success':''"></el-progress>
+                <el-col :span="12" class="label-all label_detail">
+                  <span>{{ taskMsg.imgNum}}</span>
                 </el-col>
               </div>
+
+              <!--<div>-->
+              <!--<el-col :span="10" class="label-all" style="margin-bottom: 20px">-->
+              <!--<span>任务进度:</span>-->
+              <!--</el-col>-->
+              <!--<el-col :span="12" class="label-all">-->
+              <!--<el-progress :text-inside="true" :stroke-width="18" :percentage="taskMsg.totalProgress.toFixed(2) * 100" :status="taskMsg.totalProgress===1?'success':''"></el-progress>-->
+              <!--</el-col>-->
+              <!--</div>-->
             </div>
 
-            <el-col :span="24" style="margin-bottom: 20px; margin-top:5px">
+
+            <el-col :span="24" style="margin-bottom: 20px; margin-top:23px">
               <el-col :span="6" :offset="2">
-                <el-button type="warning" size="small" @click="handleDelete">删除任务</el-button>
+                <el-button type="warning" size="medium" @click="handleAgg" disabled>删除任务</el-button>
               </el-col>
 
               <el-col :span="6" :offset="5">
-                <el-button type="primary" size="small" @click="handleFinish" :disabled="isDisabled">结束任务</el-button>
+                <el-button type="primary" size="medium" @click="handleDw">下载数据集</el-button>
               </el-col>
             </el-col>
+
 
           </el-card>
 
@@ -103,10 +118,9 @@
           <img src="../../images/ping.png" height="50" width="50" style="margin-left: -38px; margin-top: -20px">
         </el-col>
       </el-col>
-      </div>
+    </div>
 
-    </el-col>
-
+  </el-col>
 
 
 </template>
@@ -118,23 +132,23 @@
     props: ['taskMsg', 'theIndex'],
 
     computed: {
-      isDisabled(){
+      isDisabled() {
         return this.taskMsg.totalProgress >= 0.999;
       }
     },
 
     methods: {
-      handleDelete() {
-        //console.log('2line----------------------------',this.theIndex, this.taskMsg.taskID, 'finish--------------');
-        this.$emit('remove', {
-          uid: this.taskMsg.taskID,
-          index: this.theIndex
-        });
-      },
-
-      handleFinish() {
+      handleAgg() {
         //console.log('2line----------------------------',this.theIndex, this.taskMsg.taskID, 'finish--------------');
         this.$emit('complete', this.taskMsg.taskID, this.theIndex);
+      },
+
+      handleDw() {
+        //console.log('2line----------------------------',this.theIndex, this.taskMsg.taskID, 'finish--------------');
+        this.$emit('download', {
+          uid: this.taskMsg.taskID,
+          //index: this.theIndex
+        });
       },
 
       jump() {
@@ -143,7 +157,7 @@
           params: {
             taskID: this.taskMsg.taskID
           }
-      });
+        });
       }
 
     }
@@ -173,9 +187,10 @@
 
   .label-all {
     font-size: 14px;
-    margin-top: 12px;
+    margin-top: 15px;
     color: #4f4f52;
   }
+
   /*任务ID单独要放大显示*/
   .taskIDLabel {
     color: #e44030;
